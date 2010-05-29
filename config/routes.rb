b3da02b260ad65c697f62f84e61c92f2c47f5047
unknown <Ivan@.(none)>
2010-05-29 08:49:23 -0300

    Ajuste no layout dos outros models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+
+
   map.root :controller => "estorias"
   map.resources :categorias
   map.resources :estorias
+  map.resources :tarefas
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
