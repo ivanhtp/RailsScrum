32fd3f17c554ad42c2431c01be1375188fa3ce52
ivan_htp <ivanpauletti@gmail.com>
2010-09-26 21:00:53 -0300

    Ajustes no cadastro many-to-many dos sprints

--- app/models/estoria.rb
+++ app/models/estoria.rb
@@ -1,10 +1,28 @@
 class Estoria < ActiveRecord::Base
   belongs_to:categoria
   has_many :tarefas
+  has_many :sprint_estorias
   has_many :sprint, :through => :sprint_estorias
   validates_presence_of :titulo
   validates_uniqueness_of :titulo
  
+ attr_accessor :sprint_ids
+ after_save :update_sprints
+ 
+ #chamada after_save para manipular sprint_ids
+ def update_sprints
+  unless sprint_ids.nil?
+    self.sprint_estorias.each do |m|
+      m.destroy unless sprint_ids.include?(m.sprint_id.to_s)
+      sprint_ids.delete(m.sprint_id.to_s)
+    end
+    sprint_ids.each do |g|
+      self.sprint_estorias.create(:sprint_id =>g) unless g.blank?
+    end
+    reload
+    self.sprint_ids = nil
+  end
+end
 
   
 end
--- app/models/sprint_estoria.rb
+++ app/models/sprint_estoria.rb
@@ -2,3 +2,6 @@ class SprintEstoria < ActiveRecord::Base
 	belongs_to :sprint
 	belongs_to :estoria
 end
+
+#http://paulbarry.com/articles/2007/10/24/has_many-through-checkboxes
+#http://paulbarry.com/articles/2007/08/03/habtm-checkboxes
